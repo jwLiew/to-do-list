a9412a45301c13bffdd2eb9422f3a3eb964d231b
LJW <devjwliew@gmail.com>
2024-09-22 23:57:42 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const taskRoutes = require('./routes/tasks')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/tasks', taskRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONG_URI)
+   .then(() => {
+   // listen for requests
+   app.listen(process.env.PORT, () => {
+    console.log('connected to db & listening on port', process.env.PORT)
+   })
+   })
+   .catch((error) => {
+    console.log(error)
+   })
+
+
+
---
+++ frontend/src/components/TaskForm.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import { useTasksContext} from '../hooks/useTasksContext'
+
+const TaskForm = () => {
+  const { dispatch } = useTasksContext()
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [error, setError] = useState(null)
+  const [emptyFields, setEmptyFields] = useState([])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const task = {title, description}
+
+    const response = await fetch('/api/tasks', {
+        method: 'POST',
+        body: JSON.stringify(task),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+    const json = await response.json()
+
+    if(!response.ok) {
+        setError(json.error)
+        setEmptyFields(json.emptyFields)
+    }
+    if (response.ok) {
+        setTitle('')
+        setDescription('')
+        setError(null)
+        setEmptyFields([])
+        console.log('new task added', json)
+        dispatch({type: 'CREATE_TASK', payload: json})
+    }
+  }
+
+    return (
+      <form className="create" onSubmit={handleSubmit}>
+          <h3>Add a New Task</h3>
+
+          <label>Task Title:</label>
+          <input 
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          className={emptyFields.includes('title') ? 'error' : ''}
+          />
+
+          <label>Description:</label>
+          <input 
+          type="text"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          className={emptyFields.includes('description') ? 'error' : ''}
+          />
+
+          <button>Add Task</button>
+          {error && <div className="error">{error}</div>}
+      </form>
+    )
+}
+
+export default TaskForm
